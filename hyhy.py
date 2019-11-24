31fd4118f2f334c42c9c59bc18cd15b048f6ef4f
Student <iphon5spavel@gmail.com>
2019-11-24 13:14:58 +0300

    kih

---
+++ hyhy.py
@@ -0,0 +1,12 @@
+from mcpi.minecraft import Minecraft
+mc=Minecraft.create()
+
+va=0,1
+xo,yo=2,3
+a=0
+import math
+xn,yn= round(math.cos(a)),round(math.sin(a))
+while True:
+    a+=va xc,yc=round(math.cos(a)),round(math.sin(a))
+    if(xc!=xn)|(xc!=yn):
+        mc.setBlock(x0+xc,y0+yc,z0)
